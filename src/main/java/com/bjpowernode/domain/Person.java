9c78174fc0eb5df9d8a90d0724ad59ec64f36d57
zs <zs@bjpowernode.com>
2019-12-01 11:45:02 +0800

    初始导入

---
+++ src/main/java/com/bjpowernode/domain/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.domain
+ * Description:
+ *
+ * @data:2019/12/1 11:39
+ * @author:chen
+ */
+public class Person {
+    private String id;
+}
